781fdca6986b05f79a293dc56be8e20abd2efb09
kushalkanavi <kushalkanavi@gmail.com>
2017-06-07 08:28:58 -0700

    new file:   .idea/compiler.xml 	new file:   .idea/libraries/Maven__junit_junit_4_12.xml 	new file:   .idea/libraries/Maven__org_hamcrest_hamcrest_core_1_3.xml 	new file:   .idea/libraries/Maven__org_jsoup_jsoup_1_10_2.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/workspace.xml 	new file:   mywebcrawler.iml 	new file:   pom.xml 	new file:   src/main/java/crawlerEngine.java 	new file:   src/main/java/crawlerThread.java

---
+++ src/main/java/crawlerEngine.java
@@ -0,0 +1,11 @@
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Created by kushalkanavi on 6/7/17.
+ */
+public class crawlerEngine {
+
+    Queue<String> q = new ConcurrentLinkedQueue<String>();
+
+}
---
+++ src/main/java/crawlerThread.java
@@ -0,0 +1,9 @@
+/**
+ * Created by kushalkanavi on 6/7/17.
+ */
+public class crawlerThread implements Runnable{
+
+    public void run() {
+
+    }
+}
